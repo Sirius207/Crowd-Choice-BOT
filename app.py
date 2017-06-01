7ca670ac1c2a70112c8467c41de77a1690a79f35
Po Chun, Lu <oceanus11034@gmail.com>
2017-06-01 23:23:32 +0800

    print

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def _set_webhook():
 @app.route('/hook', methods=['POST'])
 def webhook_handler():
     print ('SYSTEM')
-    print machine.state
+    print (machine.state)
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     bot_route(update)
     return 'ok'
